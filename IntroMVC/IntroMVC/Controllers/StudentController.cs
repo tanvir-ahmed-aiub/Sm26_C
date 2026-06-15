56bb4ce6fa437686a4d36567b53774072e9b0b88
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2026-06-15 12:06:18 +0600

    Intro

---
+++ IntroMVC/IntroMVC/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult List() {
+            int sum =0;
+            for (int i = 1; i <= 10; i++) {
+                sum += i;
+            }
+
+
+            return View();
+        }
+        public IActionResult Create() {
+            return View();
+        }
+        public IActionResult Login() {
+            return View();
+        }
+        public IActionResult SignUp() {
+            return View();
+        }
+        public IActionResult Details(string id)
+        {
+            return View();
+        }
+}}
